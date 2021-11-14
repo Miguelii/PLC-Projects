0f6ae820e8a08b0d56c0002cfad230e2099a4dbb
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2021-11-14 22:19:08 +0000

    Add files via upload

---
+++ PLC TP1/d.py
@@ -0,0 +1,33 @@
+import json
+
+''' Suponho que seja algo assim??
+
+{
+        Processo: "582"
+        Data: "1909-05-12"
+        Nomes:
+            "Abel Almeida"
+            "Antonio Manuel Almeida
+            "Teresa Maria Sousa"
+    },
+        
+'''
+
+def d():
+    N=20
+    with open("processos.txt", "r") as fileoriginal:
+        fileN = [next(fileoriginal) for x in range(N)]
+
+    d = []
+    for v in fileN:
+        l = v.split(' :: ')
+        print(l)
+        d.append(dict(s.split(':',1) for s in l))
+
+
+    with open("json.json", 'w') as file:
+        file.write((json.dumps(d, indent=4, sort_keys= False)))
+
+
+
+d()
