a6d9e169952c20a20922b78a0b372944bdeeab94
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2021-11-14 23:43:09 +0000

    Update d.py

--- PLC TP1/d.py
+++ PLC TP1/d.py
@@ -2,14 +2,20 @@ import json
 import re
 
 ''' 
-
 "CommentThread" : [
 {
     "Processo" : "582",
     "Data" : "1909-05-12",
     "Nomes" : "Abel Almeida","Antonio Manuel Almeida", "Teresa Maria Sousa"
     },
-    
+''' 
+
+''' 
+proER = "[0-9]{3}::"
+dateER = "::1[0-9]{3}-[0-9]{2}-[0-9]{2}::"
+        for cenas in l:
+            processo = re.findall(proER, str(cenas))
+            data = re.findall(dateER, str(cenas))
 ''' 
 
 def d():
